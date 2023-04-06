afedfc652d331006b10f15ec31116e7b4b9307ad
CarolinaMacdian <115894648+CarolinaMacdian@users.noreply.github.com>
2023-04-06 14:06:24 +0100

    alterations to app

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,32 +3,33 @@
 import logo from "./images/favicon-32x32.png";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className='App'>
-      <body>
+      <p>
         How many team members can I invite? You can invite up to 2 additional
         users on the Free plan. There is no limit on team members for the
         Premium plan. What is the maximum file upload size? No more than 2GB.
         All files in your account must fit your allotted storage space. How do I
         reset my password? Click “Forgot password” from the login page or
         “Change password” from your profile page. A reset link will be emailed
-        to you. Can I cancel my subscription? Yes! Send us a message and we’ll
+        to you. Can I cancel my subscription? Yes! Send us a message and we will
         process your request no questions asked. Do you provide additional
         support? Chat and email support is available 24/7. Phone lines are open
         during normal business hours.
-        <div class='attribution'>
-          Challenge by{" "}
-          <a
-            href='https://www.frontendmentor.io?ref=challenge'
-            target='_blank'>
-            Frontend Mentor
-          </a>
-          . Coded by <a href='#'>Carolina Macdian</a>.
-        </div>
-      </body>
+      </p>
+      <div class='attribution'>
+        Challenge by{" "}
+        <a
+          href='https://www.frontendmentor.io?ref=challenge'
+          target='_blank'>
+          Frontend Mentor
+        </a>
+        . Coded by{" "}
+        <a href='https://www.frontendmentor.io?ref=challenge'>
+          Carolina Macdian
+        </a>
+      </div>
     </div>
   );
 }
-
-export default App;
